ee4dbe6ca10fd6174417f7de91884f3b7e803ab6
afshaalzubair <afshaalzubair@outlook.com>
2024-11-03 00:40:17 -0500

    meet the team section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css'
 import SplineScene from './components/SplineScene'
 import Button from './components/Button'
+import afshaalImage from './assets/afshaal.jpg';
+import kamilahImage from './assets/kamilah.png';
+import joelImage from './assets/joel.jpg';
 
 function App() {
   return (
@@ -32,9 +35,30 @@ function App() {
           <Button className="button1" label="Click to Match!" link="" />
         </div>
 
-        <section className="team-section">
+        <section className="teamheader-section">
           <h1>Meet The Team</h1>
           <p>Here is the Soul Tunes team! We are three freshmen at UT Austin studying Computer Science. The team developed this project for HackTX 2024.</p>
+        </section>
+
+        <section className="team-section">
+          <div className="team-card">
+            <img src={afshaalImage} alt="Team Member 1" />
+            <h2>Afshaal Zubair</h2>
+            <h3>Frontend Developer</h3>
+            <p>Designed the Soul Tunes intuitive interface and user experience for seamless interaction.</p>
+          </div>
+          <div className="team-card">
+            <img src={kamilahImage} alt="Team Member 2" />
+            <h2>Kamilah Clark</h2>
+            <h3>Backend Developer</h3>
+            <p>Developed the compatibility algorithm and machine learning models for playlist analysis.</p>
+          </div>
+          <div className="team-card">
+            <img src={joelImage} alt="Team Member 3" />
+            <h2>Joel Banks</h2>
+            <h3>DevOps Engineer</h3>
+            <p>Ensured compatibility between frontend and backend systems, prioritized oraganization.</p>
+          </div>
         </section>
 
       </main>
